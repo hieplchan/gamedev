6f8016f3bd35d784dadcff5fe765d96143c6d15d
Darknight <hiep.lchan@gmail.com>
2026-02-03 23:24:43 +0700

    add left arm and right arm cube

--- water6/Assets/Scenes/WaterScene/PlayerController.cs
+++ water6/Assets/Scenes/WaterScene/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed = 5f;
     [SerializeField] Rigidbody rb;
 
+    [SerializeField] GameObject leftArm;
+    [SerializeField] GameObject rightArm;
+
     private Vector2 moveInput;
     
     void OnMove(InputValue value)
