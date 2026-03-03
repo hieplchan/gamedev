37973126389d7244399f9e90cadf9f6e675b65f4
hiep.lchan <hiep.lchan@gmail.com>
2026-03-03 23:17:40 +0700

    add DOTween and animationsequencer

--- water6/Assets/Scenes/Scripts/GameManager.cs
+++ water6/Assets/Scenes/Scripts/GameManager.cs
@@ -5,19 +5,23 @@ public class GameManager : MonoBehaviour {
     [SerializeField] private GameObject player;
 
     [Header("Camera")]
-    [SerializeField] private Camera topCam;
-    [SerializeField] private Camera underWaterCam;
+    [SerializeField] private Transform topCam;
+    [SerializeField] private Transform underWaterCam;
 
     [Header("UI")]
     [SerializeField] private GameObject objUI;
     [SerializeField] private TMP_Text objUIText;
 
+    private bool _isPlayerInSwimTriggerZone = false;
+
     public void OnPlayerEnterSwimTriggerZone() {
+        _isPlayerInSwimTriggerZone = true;
         objUI.SetActive(true);
         objUIText.text = "X - Swim";
     }
 
     public void OnPlayerExitSwimTriggerZone() {
+        _isPlayerInSwimTriggerZone = false;
         objUI.SetActive(false);
     }
 }
